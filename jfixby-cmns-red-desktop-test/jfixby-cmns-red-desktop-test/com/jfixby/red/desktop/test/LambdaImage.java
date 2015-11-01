2bb630cf0a10f85ceeb3ee5db99edacefc5a83ec
J Fixby <github@jfixby.com>
2015-11-01 14:44:39 +0100

    gradient lambda function

--- jfixby-cmns-red-desktop-test/jfixby-cmns-red-desktop-test/com/jfixby/red/desktop/test/LambdaImage.java
+++ jfixby-cmns-red-desktop-test/jfixby-cmns-red-desktop-test/com/jfixby/red/desktop/test/LambdaImage.java
@@ -1,51 +1,42 @@
 package com.jfixby.red.desktop.test;
 
 import com.jfixby.cmns.desktop.DesktopAssembler;
-import com.jfixby.red.desktop.test.ExampleLambda.FunctionalInterface;
 
 public class LambdaImage {
 
-	public interface Matrix {
+	public interface Function2D {
 		float value(int x, int y);
 	}
 
-	public interface Function {
-		float value(int x, int y);
+	public interface Function2DOperator {
+		Function2D apply(Function2D f);
 	}
 
-	public interface PointOperator {
-		float value(Function f, int x0, int y0);
+	public interface Operation {
+		Function2D apply(Function2D a, Function2D b);
 	}
-
-	public interface Norm {
-		float value(VectorN n);
-	}
-
-	public interface VectorN {
-		float value(int n);
-	}
-
-	public interface Operator {
-		Function value(Function f);
-	}
-
-	static Norm factorial = null;
 
 	public static void main(String[] args) {
 		DesktopAssembler.setup();
 
-		Function image = (x, y) -> x + y;
+		Function2D image = (x, y) -> 0;
 
-		PointOperator dFx = (f, x0, y) -> (f.value(x0 + 1, y) - f.value(x0 - 1,
-				y)) / 2f;
-		PointOperator dFy = (f, x, y0) -> (f.value(x, y0 + 1) - f.value(x,
-				y0 - 1)) / 2f;
+		Function2DOperator dFx = f -> ((x, y) -> (f.value(x + 1, y) - f.value(x - 1, y)) / 2f);
+		Function2DOperator dFy = f -> ((x, y) -> (f.value(x, y + 1) - f.value(x, y - 1)) / 2f);
 
-		Operator normGradF = f -> ((x, y) -> (abs(dFx.value(f, x, y)) + abs(dFy.value(f, x, y))));
-		// PointOperator grad = (f, x0, y0) -> 0;
-		// Function frad = (x0, y0) -> x + y;
+		Function2DOperator GradXX = f -> dFx.apply(dFx.apply(f));
+		Function2DOperator GradXY = f -> dFx.apply(dFy.apply(f));
+		Function2DOperator GradYX = f -> dFy.apply(dFx.apply(f));
+		Function2DOperator GradYY = f -> dFy.apply(dFy.apply(f));
 
-		// Operator diffX = f(x,y) -> f(x + 1, y) - f(x - 1, y);
+		Operation multiply = (f, g) -> ((x, y) -> (f.value(x, y) * g.value(x, y)));
+		Operation substract = (f, g) -> ((x, y) -> (f.value(x, y) - g.value(x,y)));
+
+		Function2DOperator GardXXYY = f -> multiply.apply(GradXX.apply(f), GradYY.apply(f));
+		Function2DOperator GradXYYX = f -> multiply.apply(GradXY.apply(f), GradYX.apply(f));
+		Function2DOperator normGrad = f -> substract.apply(GardXXYY.apply(f), GradXYYX.apply(f));
+
+		Function2D sketchy = normGrad.apply(image);
 
 	}
 
