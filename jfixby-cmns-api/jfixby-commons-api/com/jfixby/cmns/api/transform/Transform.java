b995ed74432efb53f491c76d49757e8409086ba8
J Fixby <github@jfixby.com>
2015-11-19 12:41:16 +0100

    +Transform (2D)

---
+++ jfixby-cmns-api/jfixby-commons-api/com/jfixby/cmns/api/transform/Transform.java
@@ -0,0 +1,22 @@
+package com.jfixby.cmns.api.transform;
+
+import com.jfixby.cmns.api.components.ComponentInstaller;
+
+public class Transform {
+
+	static private ComponentInstaller<TransformComponent> componentInstaller = new ComponentInstaller<TransformComponent>(
+			"Transform");
+
+	public static final void installComponent(
+			TransformComponent component_to_install) {
+		componentInstaller.installComponent(component_to_install);
+	}
+
+	public static final TransformComponent invoke() {
+		return componentInstaller.invokeComponent();
+	}
+
+	public static final TransformComponent component() {
+		return componentInstaller.getComponent();
+	}
+}
---
+++ jfixby-cmns-api/jfixby-commons-api/com/jfixby/cmns/api/transform/TransformComponent.java
@@ -0,0 +1,5 @@
+package com.jfixby.cmns.api.transform;
+
+public interface TransformComponent {
+
+}
