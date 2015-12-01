100358d74a94ed4339bc4fc9908e72e4a29a7d6c
J Fixby <github@jfixby.com>
2015-12-01 12:18:13 +0100

    ...

---
+++ jfixby-cmns-red-desktop-test/jfixby-cmns-red-desktop-test/com/jfixby/red/desktop/test/GenFile.java
@@ -0,0 +1,20 @@
+package com.jfixby.red.desktop.test;
+
+import java.io.IOException;
+
+import com.jfixby.cmns.api.filesystem.File;
+import com.jfixby.cmns.api.filesystem.LocalFileSystem;
+import com.jfixby.cmns.api.log.L;
+import com.jfixby.cmns.api.math.FloatMath;
+import com.jfixby.cmns.api.math.IntegerMath;
+import com.jfixby.cmns.desktop.DesktopAssembler;
+
+public class GenFile {
+
+	public static void main(String[] args) throws IOException {
+		DesktopAssembler.setup();
+		File test_file = LocalFileSystem.ApplicationHome().child("testFile.101M");
+		test_file.writeBytes(new byte[1024 * 1024 * 101]);
+
+	}
+}
