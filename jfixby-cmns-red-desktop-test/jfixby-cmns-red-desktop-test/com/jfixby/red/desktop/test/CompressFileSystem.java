3765a0d27240d7beeaaae549f4e73997b8a33ef2
J Fixby <github@jfixby.com>
2016-01-15 15:29:17 +0100

    ...

--- jfixby-cmns-red-desktop-test/jfixby-cmns-red-desktop-test/com/jfixby/red/desktop/test/CompressFileSystem.java
+++ jfixby-cmns-red-desktop-test/jfixby-cmns-red-desktop-test/com/jfixby/red/desktop/test/CompressFileSystem.java
@@ -2,12 +2,15 @@ package com.jfixby.red.desktop.test;
 
 import java.io.IOException;
 
+import com.jfixby.cmns.adopted.gdx.json.GdxJson;
+import com.jfixby.cmns.api.collections.Collections;
 import com.jfixby.cmns.api.file.File;
 import com.jfixby.cmns.api.file.LocalFileSystem;
 import com.jfixby.cmns.api.file.packing.CompressionSchema;
 import com.jfixby.cmns.api.file.packing.FileSystemPacker;
 import com.jfixby.cmns.api.file.packing.FileSystemPackingSpecs;
 import com.jfixby.cmns.api.io.OutputStream;
+import com.jfixby.cmns.api.json.Json;
 import com.jfixby.cmns.api.log.L;
 import com.jfixby.cmns.desktop.DesktopAssembler;
 import com.jfixby.red.filesystem.archived.R3ArrayCompressionSchema;
@@ -18,6 +21,7 @@ public class CompressFileSystem {
 	public static void main(String[] args) throws IOException {
 
 		DesktopAssembler.setup();
+		Json.installComponent(new GdxJson());
 		FileSystemPacker.installComponent(new RedFileSystemPacker());
 		CompressionSchema schema = new R3ArrayCompressionSchema();
 		FileSystemPacker.installCompressionSchema(schema);
@@ -25,7 +29,7 @@ public class CompressFileSystem {
 		File folder_to_pack = LocalFileSystem.ApplicationHome().child("input");
 		File folder_compressed = LocalFileSystem.ApplicationHome().child("compressed");
 		FileSystemPackingSpecs packing_specs = FileSystemPacker.newPackingSpecs();
-		packing_specs.setTargetFolder(folder_to_pack);
+		packing_specs.setFilesList(Collections.newList(folder_to_pack));
 		File archive_file = folder_compressed.child("compressed-file-system.r3dat");
 		OutputStream os = archive_file.newOutputStream();
 		packing_specs.setOutputStream(os);
