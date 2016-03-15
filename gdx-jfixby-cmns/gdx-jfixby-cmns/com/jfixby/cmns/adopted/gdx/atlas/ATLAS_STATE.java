686a09b3e22acb585785ff6f108ff0ad17869fb0
J Fixby <github@jfixby.com>
2016-03-15 19:12:54 +0100

    ...

---
+++ gdx-jfixby-cmns/gdx-jfixby-cmns/com/jfixby/cmns/adopted/gdx/atlas/ATLAS_STATE.java
@@ -0,0 +1,5 @@
+package com.jfixby.cmns.adopted.gdx.atlas;
+
+public enum ATLAS_STATE {
+    NOT_LOADED, LOADED, BROCKEN
+}
---
+++ gdx-jfixby-cmns/gdx-jfixby-cmns/com/jfixby/cmns/adopted/gdx/atlas/CompressedFokkerAtlas.java
@@ -0,0 +1,14 @@
+package com.jfixby.cmns.adopted.gdx.atlas;
+
+import com.jfixby.cmns.api.file.File;
+
+public class CompressedFokkerAtlas extends FokkerAtlas {
+    
+    public static final String ALPHA_CHANNEL_FILE_EXTENTION = ".zipng";
+
+    public CompressedFokkerAtlas(File gdx_atlas_file) {
+	super(gdx_atlas_file);
+    }
+
+    
+}
