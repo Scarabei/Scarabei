b7583b0ea121b2c99c507bf20aaf397ae55a97dd
J Fixby <github@jfixby.com>
2016-04-03 22:53:24 +0200

    jsonn pretty print

--- jfixby-cmns-red-desktop-test/jfixby-cmns-red-desktop-test/com/jfixby/red/desktop/test/GdxJsonTest.java
+++ jfixby-cmns-red-desktop-test/jfixby-cmns-red-desktop-test/com/jfixby/red/desktop/test/GdxJsonTest.java
@@ -1,3 +1,4 @@
+
 package com.jfixby.red.desktop.test;
 
 import static org.junit.Assert.assertTrue;
@@ -13,76 +14,71 @@ import com.jfixby.red.desktop.DesktopAssembler;
 
 public class GdxJsonTest {
 
-    public long stamp = System.currentTimeMillis();
-    String test = stamp + "x";
+	public long stamp = System.currentTimeMillis();
+	String test = stamp + "x";
 
-    public Vector<String> vector = new Vector<String>();
+	public Vector<String> vector = new Vector<String>();
 
-    GdxJsonTest other = null;
+	GdxJsonTest other = null;
 
-    @Test
-    public void test() {
-	DesktopAssembler.setup();
-	Json.installComponent(new RedJson());
+	@Test
+	public void test () {
+		DesktopAssembler.setup();
+		Json.installComponent(new RedJson());
 
-	L.d("GdxJsonTest");
+		L.d("GdxJsonTest");
 
-	GdxJsonTest original = new GdxJsonTest();
-	original.vector.add(System.currentTimeMillis() + "A");
-	original.vector.add(System.currentTimeMillis() + "B");
-	original.vector.add(System.currentTimeMillis() + "C");
-	original.other = new GdxJsonTest();
+		GdxJsonTest original = new GdxJsonTest();
+		original.vector.add(System.currentTimeMillis() + "A");
+		original.vector.add(System.currentTimeMillis() + "B");
+		original.vector.add(System.currentTimeMillis() + "C");
+		original.other = new GdxJsonTest();
+		L.d("original");
+		L.d(original);
 
-	L.d("  original", original);
+		String string = Json.serializeToString(original).toString();
+		GdxJsonTest restored = Json.deserializeFromString(GdxJsonTest.class, string);
+		L.d();
+		L.d(string);
+		L.d();
+		L.d("restored", restored.equals(original));
 
-	String string = Json.serializeToString(original).toString();
-	GdxJsonTest restored = Json.deserializeFromString(GdxJsonTest.class, string);
-	L.d("  string", string);
-	L.d("restored", restored.equals(original));
+		L.d("restored");
+		L.d(restored);
+		assertTrue(restored.equals(original));
+		assertTrue(restored.toString().equals(original.toString()));
 
-	L.d("  restored", restored);
-	assertTrue(restored.equals(original));
-	assertTrue(restored.toString().equals(original.toString()));
+	}
 
-    }
+	@Override
+	public int hashCode () {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int)(stamp ^ (stamp >>> 32));
+		result = prime * result + ((test == null) ? 0 : test.hashCode());
+		result = prime * result + ((vector == null) ? 0 : vector.hashCode());
+		return result;
+	}
 
-    @Override
-    public int hashCode() {
-	final int prime = 31;
-	int result = 1;
-	result = prime * result + (int) (stamp ^ (stamp >>> 32));
-	result = prime * result + ((test == null) ? 0 : test.hashCode());
-	result = prime * result + ((vector == null) ? 0 : vector.hashCode());
-	return result;
-    }
+	@Override
+	public boolean equals (Object obj) {
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (getClass() != obj.getClass()) return false;
+		GdxJsonTest other = (GdxJsonTest)obj;
+		if (stamp != other.stamp) return false;
+		if (test == null) {
+			if (other.test != null) return false;
+		} else if (!test.equals(other.test)) return false;
+		if (vector == null) {
+			if (other.vector != null) return false;
+		} else if (!vector.equals(other.vector)) return false;
+		return true;
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-	if (this == obj)
-	    return true;
-	if (obj == null)
-	    return false;
-	if (getClass() != obj.getClass())
-	    return false;
-	GdxJsonTest other = (GdxJsonTest) obj;
-	if (stamp != other.stamp)
-	    return false;
-	if (test == null) {
-	    if (other.test != null)
-		return false;
-	} else if (!test.equals(other.test))
-	    return false;
-	if (vector == null) {
-	    if (other.vector != null)
-		return false;
-	} else if (!vector.equals(other.vector))
-	    return false;
-	return true;
-    }
-
-    @Override
-    public String toString() {
-	return "GdxJsonTest [stamp=" + stamp + ", test=" + test + ", vector=" + vector + "]";
-    }
+	@Override
+	public String toString () {
+		return "GdxJsonTest [stamp=" + stamp + ", test=" + test + ", vector=" + vector + "]";
+	}
 
 }
