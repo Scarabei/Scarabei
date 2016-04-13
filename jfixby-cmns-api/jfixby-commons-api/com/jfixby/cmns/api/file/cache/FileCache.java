3e4c177f30166e8a830dc20195bc5f9fb1594ac3
J Fixby <github@jfixby.com>
2016-04-13 12:54:58 +0200

    ...

--- jfixby-cmns-api/jfixby-commons-api/com/jfixby/cmns/api/file/cache/FileCache.java
+++ jfixby-cmns-api/jfixby-commons-api/com/jfixby/cmns/api/file/cache/FileCache.java
@@ -1,3 +1,4 @@
+
 package com.jfixby.cmns.api.file.cache;
 
 import com.jfixby.cmns.api.ComponentInstaller;
@@ -5,27 +6,27 @@ import com.jfixby.cmns.api.file.File;
 
 public class FileCache {
 
-    static private ComponentInstaller<FileCacheComponent> componentInstaller = new ComponentInstaller<FileCacheComponent>(
-	    "FileCache");
+	static private ComponentInstaller<FileCacheComponent> componentInstaller = new ComponentInstaller<FileCacheComponent>(
+		"FileCache");
 
-    public static final void installComponent(FileCacheComponent component_to_install) {
-	componentInstaller.installComponent(component_to_install);
-    }
+	public static final void installComponent (final FileCacheComponent component_to_install) {
+		componentInstaller.installComponent(component_to_install);
+	}
 
-    public static final FileCacheComponent invoke() {
-	return componentInstaller.invokeComponent();
-    }
+	public static final FileCacheComponent invoke () {
+		return componentInstaller.invokeComponent();
+	}
 
-    public static final FileCacheComponent component() {
-	return componentInstaller.getComponent();
-    }
+	public static final FileCacheComponent component () {
+		return componentInstaller.getComponent();
+	}
 
-    public static TempFolder createTempFolder(File where) {
-	return invoke().createTempFolder(where);
-    }
+	public static TempFolder createTempFolder (final File where) {
+		return invoke().createTempFolder(where);
+	}
 
-    public static TempFolder createTempFolder() {
-	return invoke().createTempFolder();
-    }
+	public static TempFolder createTempFolder () {
+		return invoke().createTempFolder();
+	}
 
 }
