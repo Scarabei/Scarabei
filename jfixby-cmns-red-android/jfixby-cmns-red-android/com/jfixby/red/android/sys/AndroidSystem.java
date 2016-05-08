8843d957491785f61d05bfbd8aacadf3cbb0ae01
J Fixby <github@jfixby.com>
2016-05-08 22:05:02 +0200

    ...

--- jfixby-cmns-red-android/jfixby-cmns-red-android/com/jfixby/red/android/sys/AndroidSystem.java
+++ jfixby-cmns-red-android/jfixby-cmns-red-android/com/jfixby/red/android/sys/AndroidSystem.java
@@ -1,23 +1,22 @@
 
 package com.jfixby.red.android.sys;
 
-import com.jfixby.cmns.api.log.L;
 import com.jfixby.red.sys.RedSystem;
 
 public class AndroidSystem extends RedSystem {
 
 	@Override
 	public void exit () {
-		L.d("EXIT");
+		System.out.println("EXIT");
 		System.exit(0);
 	}
 
 	@Override
-	public boolean sleep (long period) {
+	public boolean sleep (final long period) {
 		try {
 			Thread.sleep(period);
 			return true;
-		} catch (InterruptedException e) {
+		} catch (final InterruptedException e) {
 			e.printStackTrace();
 			return false;
 		}
