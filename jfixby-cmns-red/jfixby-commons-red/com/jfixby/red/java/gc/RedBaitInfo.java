fedabea79a602f3960f513a82c7cc969cf5a1a3c
J Fixby <github@jfixby.com>
2016-05-28 21:03:29 +0200

    CGBait

--- jfixby-cmns-red/jfixby-commons-red/com/jfixby/red/java/gc/RedBaitInfo.java
+++ jfixby-cmns-red/jfixby-commons-red/com/jfixby/red/java/gc/RedBaitInfo.java
@@ -8,18 +8,18 @@ public class RedBaitInfo implements BaitInfo {
 
 	@Override
 	public String toString () {
-		return "RedBaitInfo [bait_id=" + bait_id + ", timestamp=" + timestamp + ", size=" + size() + "]";
+		return "BaitInfo [bait_id=" + this.bait_id + ", timestamp=" + this.timestamp + ", size=" + this.size() + "]";
 	}
 
 	private String size () {
-		return FloatMath.roundToDigit(size * 1f / (1024 * 1024), 3) + " Mb";
+		return FloatMath.roundToDigit(this.size * 1f / (1024 * 1024), 3) + " Mb";
 	}
 
-	private long timestamp;
-	private long bait_id;
-	private int size;
+	private final long timestamp;
+	private final long bait_id;
+	private final int size;
 
-	public RedBaitInfo (RedBait redBait) {
+	public RedBaitInfo (final RedBait redBait) {
 		this.timestamp = redBait.timestamp;
 		this.bait_id = redBait.bait_id;
 		this.size = redBait.BAIT_SIZE;
