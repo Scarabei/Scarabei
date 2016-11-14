36a3c3ad7ecb7320dbdb4c051e8c793d7392fdfc
J Fixby <github@jfixby.com>
2016-11-14 17:54:11 +0100

    ...

--- jfixby-cmns-red-android/jfixby-cmns-red-android/com/jfixby/red/android/AndroidSetup.java
+++ jfixby-cmns-red-android/jfixby-cmns-red-android/com/jfixby/red/android/AndroidSetup.java
@@ -16,7 +16,6 @@ import com.jfixby.cmns.api.file.cache.FileCache;
 import com.jfixby.cmns.api.geometry.Geometry;
 import com.jfixby.cmns.api.graphs.Graphs;
 import com.jfixby.cmns.api.image.ImageProcessing;
-import com.jfixby.cmns.api.input.UserInput;
 import com.jfixby.cmns.api.io.IO;
 import com.jfixby.cmns.api.java.gc.GCFisher;
 import com.jfixby.cmns.api.log.L;
@@ -25,6 +24,7 @@ import com.jfixby.cmns.api.math.IntegerMath;
 import com.jfixby.cmns.api.math.MathTools;
 import com.jfixby.cmns.api.math.SimpleTriangulator;
 import com.jfixby.cmns.api.md5.MD5;
+import com.jfixby.cmns.api.memory.MemoryManager;
 import com.jfixby.cmns.api.net.http.Http;
 import com.jfixby.cmns.api.random.Random;
 import com.jfixby.cmns.api.sys.Sys;
@@ -35,6 +35,7 @@ import com.jfixby.red.android.collections.AndroidCollections;
 import com.jfixby.red.android.filesystem.AndroidFileSystem;
 import com.jfixby.red.android.log.AndroidLogger;
 import com.jfixby.red.android.math.AndroidFloatMath;
+import com.jfixby.red.android.memory.AndroidMemoryManager;
 import com.jfixby.red.android.sys.AndroidSystem;
 import com.jfixby.red.arrays.RedArrays;
 import com.jfixby.red.color.RedColors;
@@ -44,7 +45,6 @@ import com.jfixby.red.filesystem.cache.RedFileCache;
 import com.jfixby.red.geometry.RedGeometry;
 import com.jfixby.red.graphs.RedGraphs;
 import com.jfixby.red.image.RedImageProcessing;
-import com.jfixby.red.input.RedInput;
 import com.jfixby.red.io.RedIO;
 import com.jfixby.red.java.gc.RedGCFisher;
 import com.jfixby.red.math.RedAngles;
@@ -79,6 +79,12 @@ public class AndroidSetup {
 		MathTools.installComponent(new RedMathTools());
 		Err.installComponent(new RedError());
 		Debug.installComponent(new RedDebug());
+
+		Android.installComponent(android);
+		MemoryManager.installComponent(new AndroidMemoryManager());
+		L.d("Max heap size: ", MemoryManager.getMaxHeapSize() + "Mb");
+		L.d("Recommended heap size: ", MemoryManager.getRecommendedHeapSize() + "Mb");
+
 		GCFisher.installComponent(new RedGCFisher());
 
 		MD5.installComponent(new RSADataSecurityIncMD5());
@@ -87,9 +93,8 @@ public class AndroidSetup {
 
 		SimpleTriangulator.installComponent(new GdxSimpleTriangulator());
 
-		UserInput.installComponent(new RedInput());
 		{
-			Android.installComponent(android);
+
 			LocalFileSystem.installComponent(new AndroidFileSystem());
 		}
 
