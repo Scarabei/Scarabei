170f1eb96a7bb2066e75d0413b54b57700eb4f73
J Fixby <github@jfixby.com>
2016-11-22 18:21:10 +0100

    +jfixby-cmns-api-db

---
+++ jfixby-cmns-api-db/src/com/jfixby/cmns/api/db/DatabaseClient.java
@@ -0,0 +1,6 @@
+
+package com.jfixby.cmns.api.db;
+
+public interface DatabaseClient {
+
+}
--- jfixby-cmns-red/jfixby-commons-red/com/jfixby/red/net/RedHttpURL.java
+++ jfixby-cmns-red/jfixby-commons-red/com/jfixby/red/net/RedHttpURL.java
@@ -14,49 +14,22 @@ import com.jfixby.cmns.api.util.path.RelativePath;
 
 public class RedHttpURL implements HttpURL {
 
-	@Override
-	public int hashCode () {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((this.java_url == null) ? 0 : this.java_url.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals (final Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null) {
-			return false;
-		}
-		if (this.getClass() != obj.getClass()) {
-			return false;
-		}
-		final RedHttpURL other = (RedHttpURL)obj;
-		if (this.java_url == null) {
-			if (other.java_url != null) {
-				return false;
-			}
-		} else if (!this.java_url.equals(other.java_url)) {
-			return false;
-		}
-		return true;
-	}
-
 	// private final String url_string;
 	private URL java_url;
 	private RelativePath relativeServer;
 	private AbsolutePath<Host> abs;
 	private String rootString;
 	private RedHost host;
+	private String url_string;
 
 	public RedHttpURL (final String url_string) {
 		Debug.checkEmpty("url_string", url_string);
 		Debug.checkNull("url_string", url_string);
 // this.url_string = url_string;
 		try {
+
 			this.java_url = new URL(url_string);
+			this.url_string = this.java_url.toString();
 			this.rootString = getURLRoot(this.java_url);
 			final String path = this.java_url.getPath();
 			this.relativeServer = JUtils.newRelativePath(path);
@@ -75,12 +48,12 @@ public class RedHttpURL implements HttpURL {
 
 	@Override
 	public String toString () {
-		return this.java_url + "";
+		return this.url_string;
 	}
 
 	@Override
 	public String getURLString () {
-		return this.java_url + "";
+		return this.url_string;
 	}
 
 	@Override
@@ -121,6 +94,36 @@ public class RedHttpURL implements HttpURL {
 	@Override
 	public boolean beginsWith (final AbsolutePath<? extends Host> rootPath) {
 		return this.abs.beginsWith(rootPath);
+	}
+
+	@Override
+	public int hashCode () {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((this.url_string == null) ? 0 : this.url_string.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals (final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (this.getClass() != obj.getClass()) {
+			return false;
+		}
+		final RedHttpURL other = (RedHttpURL)obj;
+		if (this.url_string == null) {
+			if (other.url_string != null) {
+				return false;
+			}
+		} else if (!this.url_string.equals(other.url_string)) {
+			return false;
+		}
+		return true;
 	}
 
 }
