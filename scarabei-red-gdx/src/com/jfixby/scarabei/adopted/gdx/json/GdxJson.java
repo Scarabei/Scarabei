e19f79be9f28aa3681a56647e6d86fd4235c74de
J Fixby <github@jfixby.com>
2017-01-25 18:21:28 +0100

    deprecate GdxJson

--- scarabei-red-gdx/src/com/jfixby/scarabei/adopted/gdx/json/GdxJson.java
+++ scarabei-red-gdx/src/com/jfixby/scarabei/adopted/gdx/json/GdxJson.java
@@ -4,6 +4,6 @@ package com.jfixby.scarabei.adopted.gdx.json;
 import com.jfixby.scarabei.adopted.gdx.json.red.RedGdxJson;
 import com.jfixby.scarabei.api.json.JsonComponent;
 
-public class RedJson extends RedGdxJson implements JsonComponent {
+class GdxJson extends RedGdxJson implements JsonComponent {
 
 }
