315e187d3fa8c2034f7d914bcb7e95037723f48b
J Fixby <github@jfixby.com>
2017-01-25 18:34:15 +0100

    ...

--- scarabei-red-gdx/src/com/jfixby/scarabei/adopted/gdx/json/GdxJson.java
+++ scarabei-red-gdx/src/com/jfixby/scarabei/adopted/gdx/json/GdxJson.java
@@ -4,6 +4,6 @@ package com.jfixby.scarabei.adopted.gdx.json;
 import com.jfixby.scarabei.adopted.gdx.json.red.RedGdxJson;
 import com.jfixby.scarabei.api.json.JsonComponent;
 
-class GdxJson extends RedGdxJson implements JsonComponent {
+public class GdxJson extends RedGdxJson implements JsonComponent {
 
 }
