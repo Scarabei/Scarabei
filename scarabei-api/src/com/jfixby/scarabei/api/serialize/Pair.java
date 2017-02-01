4f2e95a5ac2c0278ff18fd0827cf3534921faf43
JFixby <github@jfixby.com>
2017-02-01 17:28:46 +0100

    deprecate serialize

---
+++ scarabei-api/src/com/jfixby/scarabei/api/serialize/Pair.java
@@ -0,0 +1,9 @@
+//
+// package com.jfixby.scarabei.api.serialize;
+//
+// public class Pair<T> {
+//
+// public String key;
+// public T value;
+//
+// }
--- scarabei-red/src/com/jfixby/scarabei/red/serialize/RedSerialize.java
+++ scarabei-red/src/com/jfixby/scarabei/red/serialize/RedSerialize.java
@@ -1,18 +1,41 @@
-
-package com.jfixby.scarabei.red.serialize;
-
-import java.util.HashMap;
-
-import com.jfixby.scarabei.api.collections.Map;
-import com.jfixby.scarabei.api.serialize.SerializeComponent;
-
-public class RedSerialize implements SerializeComponent {
-
-	@Override
-	public <P, Q> HashMap<P, Q> serialize (final Map<P, Q> input) {
-		final HashMap<P, Q> result = new HashMap<P, Q>();
-		result.putAll(input.toJavaMap());
-		return result;
-	}
-
-}
+//
+// package com.jfixby.scarabei.red.serialize;
+//
+// import com.jfixby.scarabei.api.collections.Collections;
+// import com.jfixby.scarabei.api.collections.Map;
+// import com.jfixby.scarabei.api.serialize.MappingList;
+// import com.jfixby.scarabei.api.serialize.Pair;
+// import com.jfixby.scarabei.api.serialize.SerializeComponent;
+//
+// public class RedSerialize implements SerializeComponent {
+//
+// @Override
+// public <P, Q> MappingList<Q> serialize (final Map<P, Q> input) {
+// final MappingList<Q> result = new MappingList<Q>();
+// for (final P key : input.keys()) {
+//// new Pair(key, input.get(key))
+// final Pair<Q> pair = new Pair<Q>();
+// if (key != null) {
+// pair.key = key.toString();
+// }
+// pair.value = input.get(key);
+// result.list.add(pair);
+// }
+// return result;
+// }
+//
+// @Override
+// public <Q> Map<String, Q> deSerialize (final MappingList<Q> mapping, final Map<String, Q> map) {
+// for (final Pair<Q> p : mapping.list) {
+// map.put(p.key, p.value);
+// }
+// return map;
+// }
+//
+// @Override
+// public <Q> Map<String, Q> deSerialize (final MappingList<Q> mapping) {
+// final Map<String, Q> result = Collections.newMap();
+// return this.deSerialize(mapping, result);
+// }
+//
+// }
