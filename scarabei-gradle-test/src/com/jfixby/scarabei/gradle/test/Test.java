998f58e61778e93bb03128c9a985c4e642eac45b
J Fixby <github@jfixby.com>
2017-03-10 14:53:21 +0100

    +gradle-test project

---
+++ scarabei-gradle-test/src/com/jfixby/scarabei/gradle/test/Test.java
@@ -0,0 +1,10 @@
+
+package com.jfixby.scarabei.gradle.test;
+
+public class Test {
+
+	public static void main (final String[] args) {
+		final com.jfixby.scarabei.api.file.File file = null;
+	}
+
+}
---
+++ scarabei-gradle-test/src/com/jfixby/scarabei/gradle/test/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author JCode
+ *
+ */
+
+package com.jfixby.scarabei.gradle.test;
