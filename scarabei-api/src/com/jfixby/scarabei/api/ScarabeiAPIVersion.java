26ee38d04d8894f2d5e9f6e46122aa23a0a245f4
J Fixby <github@jfixby.com>
2017-03-13 11:10:18 +0100

    1.2.5.2

---
+++ scarabei-api/src/com/jfixby/scarabei/api/ScarabeiAPIVersion.java
@@ -0,0 +1,8 @@
+
+package com.jfixby.scarabei.api;
+
+public class ScarabeiAPIVersion {
+
+	public static final String versionString = "Scarabei-1.2.5.2";
+
+}
---
+++ scarabei-red-desktop-test/src/com/jfixby/scarabei/red/desktop/test/VersionCheck.java
@@ -0,0 +1,15 @@
+
+package com.jfixby.scarabei.red.desktop.test;
+
+import com.jfixby.scarabei.api.desktop.ScarabeiDesktop;
+import com.jfixby.scarabei.api.log.L;
+
+public class VersionCheck {
+
+	public static void main (final String[] args) {
+		ScarabeiDesktop.deploy();
+		L.d(ScarabeiAPIVersion.versionString);
+
+	}
+
+}
