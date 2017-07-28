32be35e8c417e576ac4b1749120f18d7567b8d28
J Fixby <github@jfixby.com>
2017-07-28 16:16:06 +0200

    java map

--- scarabei-red-android-flutter/src/com/jfixby/scarabei/red/flutter/plugins/android/ScarabeiAndroidPlugin.java
+++ scarabei-red-android-flutter/src/com/jfixby/scarabei/red/flutter/plugins/android/ScarabeiAndroidPlugin.java
@@ -4,6 +4,7 @@ package com.jfixby.scarabei.red.flutter.plugins.android;
 import com.jfixby.scarabei.android.api.Android;
 import com.jfixby.scarabei.api.flutter.plugins.FlutterPluginSpecs;
 import com.jfixby.scarabei.api.log.L;
+import com.jfixby.scarabei.api.sys.SystemInfo;
 
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
@@ -16,7 +17,8 @@ public class ScarabeiAndroidPlugin implements MethodCallHandler {
 		L.d("ScarabeiAndroidPlugin.onMethodCall", call.method);
 		if (call.method.equals("getSystemInfo")) {
 // final String message = "Android " + android.os.Build.VERSION.RELEASE + " " + Android.getSystemInfo();
-			result.success(Android.getSystemInfo());
+			final SystemInfo systemInfo = Android.getSystemInfo();
+			result.success(systemInfo.listParameters().toJavaMap());
 		} else {
 			result.notImplemented();
 		}
