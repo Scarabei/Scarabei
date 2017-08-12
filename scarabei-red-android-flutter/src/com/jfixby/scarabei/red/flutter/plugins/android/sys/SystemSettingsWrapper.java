9981b498aa0971026a500a1117f6902443b491ec
J Fixby <github@jfixby.com>
2017-08-13 01:45:26 +0200

    static call

--- scarabei-red-android-flutter/src/com/jfixby/scarabei/red/flutter/plugins/android/sys/SystemSettingsWrapper.java
+++ scarabei-red-android-flutter/src/com/jfixby/scarabei/red/flutter/plugins/android/sys/SystemSettingsWrapper.java
@@ -8,63 +8,63 @@ import com.jfixby.scarabei.api.log.L;
 import com.jfixby.scarabei.api.sys.settings.ExecutionMode;
 import com.jfixby.scarabei.api.sys.settings.SystemSettings;
 
-public class SystemSettingsWrapper {
+public static class SystemSettingsWrapper {
 
-	public void setExecutionMode (final ExecutionMode execution_mode) {
+	public static void setExecutionMode (final ExecutionMode execution_mode) {
 		SystemSettings.setExecutionMode(execution_mode);
 	}
 
-	public void setFlag (final ID flag_name, final boolean flag_value) {
+	public static void setFlag (final ID flag_name, final boolean flag_value) {
 		SystemSettings.setFlag(flag_name, flag_value);
 	}
 
-	public boolean getFlag (final ID flag_name) {
+	public static boolean getFlag (final ID flag_name) {
 		return SystemSettings.getFlag(flag_name);
 	}
 
-	public String getStringParameter (final ID parameter_name, final String defaultValue) {
+	public static String getStringParameter (final ID parameter_name, final String defaultValue) {
 		return SystemSettings.getStringParameter(parameter_name, defaultValue);
 	}
 
-	public void setStringParameter (final ID parameter_name, final String parameter_value) {
+	public static void setStringParameter (final ID parameter_name, final String parameter_value) {
 		SystemSettings.setStringParameter(parameter_name, parameter_value);
 	}
 
-	public void setSystemAssetID (final ID parameter_name, final ID parameter_value) {
+	public static void setSystemAssetID (final ID parameter_name, final ID parameter_value) {
 		SystemSettings.setSystemAssetID(parameter_name, parameter_value);
 	}
 
-	public String getSystemAssetID (final ID parameter_name) {
+	public static String getSystemAssetID (final ID parameter_name) {
 		return SystemSettings.getSystemAssetID(parameter_name) + "";
 	}
 
-	public boolean executionModeIsAtLeast (final ExecutionMode mode) {
+	public static boolean executionModeIsAtLeast (final ExecutionMode mode) {
 		return SystemSettings.executionModeIsAtLeast(mode);
 	}
 
-	public String getExecutionMode () {
+	public static String getExecutionMode () {
 		return SystemSettings.getExecutionMode() + "";
 	}
 
-	public void setIntParameter (final ID parameterName, final long parameterValue) {
+	public static void setIntParameter (final ID parameterName, final long parameterValue) {
 		SystemSettings.setIntParameter(parameterName, parameterValue);
 	}
 
-	public long getIntParameter (final ID parameter_name) {
+	public static long getIntParameter (final ID parameter_name) {
 		return SystemSettings.getIntParameter(parameter_name);
 	}
 
-	public java.util.Map<String, Object> listAllSettings () {
+	public static java.util.Map<String, Object> listAllSettings () {
 		final Map<ID, Object> list = SystemSettings.listAllSettings();
 		final Map<String, Object> map = Collections.newMap();
 		for (final ID key : list.keys()) {
-			map.put(key + "", this.value(list.get(key)));
+			map.put(key + "", value(list.get(key)));
 
 		}
 		return map.toJavaMap();
 	}
 
-	private Object value (final Object object) {
+	public static Object value (final Object object) {
 		if (object instanceof ExecutionMode) {
 			return object + "";
 		}
@@ -74,11 +74,11 @@ public class SystemSettingsWrapper {
 		return object;
 	}
 
-	public void clearAll () {
+	public static void clearAll () {
 		SystemSettings.clearAll();
 	}
 
-	public boolean saveToStorage () {
+	public static boolean saveToStorage () {
 		try {
 			return SystemSettings.saveToStorage().await();
 		} catch (final Throwable e) {
