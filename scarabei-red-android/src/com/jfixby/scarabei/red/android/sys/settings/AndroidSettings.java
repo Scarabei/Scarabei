e4529c1ccc877cc6d2bd2a2306b18b439fc50028
J Fixby <github@jfixby.com>
2017-09-29 10:22:32 +0200

    fix prefs

--- scarabei-red-android/src/com/jfixby/scarabei/red/android/sys/settings/AndroidSettings.java
+++ scarabei-red-android/src/com/jfixby/scarabei/red/android/sys/settings/AndroidSettings.java
@@ -20,42 +20,46 @@ public class AndroidSettings implements SystemSettingsComponent {
 	private final android.content.SharedPreferences preferences;
 	private final android.content.SharedPreferences.Editor prefs;
 
-	public AndroidSettings () {
+	public AndroidSettings() {
 		final Context context = Android.getApplicationContext();
-		this.preferences = context.getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE);
+		this.preferences = context.getSharedPreferences(AndroidSettings.SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE);
 		this.prefs = this.preferences.edit();
 	}
 
 	@Override
-	public boolean executionModeIsAtLeast (final ExecutionMode execution_mode) {
+	public boolean executionModeIsAtLeast(final ExecutionMode execution_mode) {
 		return this.getExecutionMode().isAtLeast(execution_mode);
 	}
 
 	@Override
-	public ExecutionMode getExecutionMode () {
-		return ExecutionMode
-			.resolve(this.getStringParameter(ExecutionMode.ExecutionModeTAG(), ExecutionMode.PUBLIC_RELEASE.toString()));
+	public ExecutionMode getExecutionMode() {
+		return ExecutionMode.resolve(
+				this.getStringParameter(ExecutionMode.ExecutionModeTAG(), ExecutionMode.PUBLIC_RELEASE.toString()));
 	}
 
 	@Override
-	public void setExecutionMode (final ExecutionMode executionMode) {
+	public void setExecutionMode(final ExecutionMode executionMode) {
 		Debug.checkNull("ExecutionMode", executionMode);
 		this.prefs.putString(ExecutionMode.ExecutionModeTAG().toString(), executionMode.toString()).apply();
 		this.prefs.commit();
 	}
 
 	@Override
-	public Map<ID, Object> listAllSettings () {
+	public Map<ID, Object> listAllSettings() {
 		final Map<ID, Object> params = Collections.newMap();
 		final java.util.Map<String, ?> allPrefs = this.preferences.getAll();
 		for (final String key : allPrefs.keySet()) {
-			params.put(Names.newID(key), allPrefs.get(key));
+			Object value = allPrefs.get(key);
+			if (ExecutionMode.ExecutionModeTAG().toString().equals(key)) {
+				value = ExecutionMode.resolve("" + value);
+			}
+			params.put(Names.newID(key), value);
 		}
 		return params;
 	}
 
 	@Override
-	public void clearAll () {
+	public void clearAll() {
 		final java.util.Map<String, ?> allPrefs = this.preferences.getAll();
 		for (final String key : allPrefs.keySet()) {
 			this.prefs.remove(key);
@@ -63,24 +67,24 @@ public class AndroidSettings implements SystemSettingsComponent {
 	}
 
 	@Override
-	public void setFlag (final ID flag_name, final boolean flag_value) {
+	public void setFlag(final ID flag_name, final boolean flag_value) {
 		this.prefs.putBoolean(flag_name.toString(), flag_value).apply();
 		this.prefs.commit();
 	}
 
 	@Override
-	public boolean getFlag (final ID flag_name) {
+	public boolean getFlag(final ID flag_name) {
 		return this.preferences.getBoolean(flag_name.toString(), false);
 	}
 
 	@Override
-	public String getStringParameter (final ID parameter_name, final String defaultValue) {
+	public String getStringParameter(final ID parameter_name, final String defaultValue) {
 		final String value = this.preferences.getString(parameter_name.toString(), defaultValue);
 		return value;
 	}
 
 	@Override
-	public ID getSystemAssetID (final ID parameter_name) {
+	public ID getSystemAssetID(final ID parameter_name) {
 		final String value = this.preferences.getString(parameter_name.toString(), null);
 		if (value == null) {
 			L.e("Parameter not found", parameter_name);
@@ -94,18 +98,18 @@ public class AndroidSettings implements SystemSettingsComponent {
 	}
 
 	@Override
-	public long getIntParameter (final ID parameterName) {
+	public long getIntParameter(final ID parameterName) {
 		return this.preferences.getLong(parameterName.toString(), 0L);
 	}
 
 	@Override
-	public void setStringParameter (final ID parameter_name, final String parameter_value) {
+	public void setStringParameter(final ID parameter_name, final String parameter_value) {
 		this.prefs.putString(parameter_name.toString(), parameter_value).apply();
 		this.prefs.commit();
 	}
 
 	@Override
-	public void setSystemAssetID (final ID parameter_name, final ID parameter_value) {
+	public void setSystemAssetID(final ID parameter_name, final ID parameter_value) {
 		if (parameter_value != null) {
 			this.prefs.putString(parameter_name.toString(), parameter_value.toString()).apply();
 			this.prefs.commit();
@@ -116,13 +120,13 @@ public class AndroidSettings implements SystemSettingsComponent {
 	}
 
 	@Override
-	public void setIntParameter (final ID parameterName, final long parameterValue) {
+	public void setIntParameter(final ID parameterName, final long parameterValue) {
 		this.prefs.putLong(parameterName.toString(), parameterValue).apply();
 		this.prefs.commit();
 	}
 
 	@Override
-	public boolean saveToStorage () {
+	public boolean saveToStorage() {
 		return this.prefs.commit();
 	}
 
