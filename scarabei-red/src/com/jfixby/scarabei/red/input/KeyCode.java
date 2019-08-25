6fc631e601ceacdf9d1ada5096c9b23f0965667e
J Fixby <github@jfixby.com>
2019-08-25 15:38:47 +0200

    repush

--- scarabei-red/src/com/jfixby/scarabei/red/input/KeyCode.java
+++ scarabei-red/src/com/jfixby/scarabei/red/input/KeyCode.java
@@ -6,19 +6,25 @@ import com.jfixby.scarabei.api.input.Key;
 public final class KeyCode implements Key {
 	final String name;
 
-	KeyCode (String name, RedKeyboard keyboard) {
+	KeyCode (final String name, final RedKeyboard keyboard) {
 		this.name = name;
-		final RedKeysList list = (RedKeysList)keyboard.KEYS_LIST;
+		final RedKeysList list = keyboard.KEYS_LIST;
 		list.add(this);
 	}
 
 	@Override
 	public String toString () {
-		return "KeyCode [" + name + "]";
+		return "KeyCode [" + this.name + "]";
 	}
 
+	@Override
 	public String getName () {
 		return this.name;
+	}
+
+	@Override
+	public boolean is (final Key other) {
+		return this == other;
 	}
 
 }
